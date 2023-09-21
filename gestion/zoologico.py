a29729d35b1f94d168a88c162e2e274c3f6b5820
Mnico-11 <141962291+Mnico-11@users.noreply.github.com>
2023-09-21 17:26:27 -0500

    =)

--- gestion/zoologico.py
+++ gestion/zoologico.py
@@ -1,21 +1,12 @@
-import zooAnimales
-from zooAnimales.animal import Animal
-    
-class Zoologico():
+class Zoologico:
     def __init__(self, nombre, ubicacion):
         self._nombre = nombre
-        self._ubicacion = ubicacion
-        self._zona = []
-
-    def agregarZonas(self, zona):
-        self._zona.append(zona)
-
-    def cantidadTotalAnimales(self):
-        return Animal.getTotalAnimales()
-    
+        self._ubicacion: ubicacion
+        self._zonas = []
+        
     def getNombre(self):
         return self._nombre
-
+    
     def setNombre(self, nombre):
         self._nombre = nombre
     
@@ -24,9 +15,18 @@ class Zoologico():
     
     def setUbicacion(self, ubicacion):
         self._ubicacion = ubicacion
-
+        
     def getZona(self):
-        return self._zona
+        return self._zonas
     
-    def setZona(self, zona):
-        self._zona = zona
+    def setZonas(self, zonas):
+        self._zonas = zonas
+        
+    def agregarZonas(self, zona):
+        self._zonas.append(zona)
+    
+    def cantidadTotalAnimales(self):
+        totalAnimales = 0
+        for zona in self._zonas:
+            totalAnimales += zona.cantidadAnimales()
+        return totalAnimales
